a58ac8a1d821e4bca1ed8bb7642f4623ae94ea5d
JackScott808 <jackscott808@gmail.com>
2024-07-31 16:11:37 +0100

    Changed IP logic to be dynamic

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
-# config.py
-import os
 import socket
 
 PI_1_IP = '192.168.245.200'
 PI_2_IP = '192.168.245.64'
 
 def get_local_and_peer_ip():
-    local_ip = PI_1_IP if PI_1_IP in socket.gethostbyname_ex(socket.gethostname())[2] else PI_2_IP
-    peer_ip = PI_2_IP if local_ip == PI_1_IP else PI_1_IP
-    return local_ip, peer_ip
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    if local_ip == PI_1_IP:
+        return PI_1_IP, PI_2_IP
+    else:
+        return PI_2_IP, PI_1_IP
 
 LOCAL_IP, PEER_IP = get_local_and_peer_ip()
