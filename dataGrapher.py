30e29c28211c037c35a285a7d41842519af36af9
JackScott808 <jackscott808@gmail.com>
2024-08-02 16:05:14 +0100

    Added 2x2 grid to plotter

--- dataGrapher.py
+++ dataGrapher.py
@@ -17,31 +17,34 @@ df = pd.read_csv(latest_file, parse_dates=['Timestamp'])
 df.set_index('Timestamp', inplace=True)
 
 # Create a figure with subplots
-fig, axs = plt.subplots(3, 1, figsize=(12, 15), sharex=True)
+fig, axs = plt.subplots(2, 2, figsize=(12, 8), sharex=True)
 
 # Plot Solar and Battery Bus Voltage
-axs[0].plot(df.index, df['Solar Bus Voltage (V)'], label='Solar')
-axs[0].plot(df.index, df['Battery Bus Voltage (V)'], label='Battery')
-axs[0].set_ylabel('Bus Voltage (V)')
-axs[0].legend()
-axs[0].set_title('Bus Voltage over Time')
+axs[0, 0].plot(df.index, df['Solar Bus Voltage (V)'], label='Solar')
+axs[0, 0].plot(df.index, df['Battery Bus Voltage (V)'], label='Battery')
+axs[0, 0].set_ylabel('Bus Voltage (V)')
+axs[0, 0].legend()
+axs[0, 0].set_title('Bus Voltage over Time')
 
 # Plot Solar and Battery Current
-axs[1].plot(df.index, df['Solar Current (A)'], label='Solar')
-axs[1].plot(df.index, df['Battery Current (A)'], label='Battery')
-axs[1].set_ylabel('Current (A)')
-axs[1].legend()
-axs[1].set_title('Current over Time')
+axs[0, 1].plot(df.index, df['Solar Current (A)'], label='Solar')
+axs[0, 1].plot(df.index, df['Battery Current (A)'], label='Battery')
+axs[0, 1].set_ylabel('Current (A)')
+axs[0, 1].legend()
+axs[0, 1].set_title('Current over Time')
 
 # Plot Solar and Battery Power
-axs[2].plot(df.index, df['Solar Power (mW)'], label='Solar')
-axs[2].plot(df.index, df['Battery Power (mW)'], label='Battery')
-axs[2].set_ylabel('Power (mW)')
-axs[2].legend()
-axs[2].set_title('Power over Time')
+axs[1, 0].plot(df.index, df['Solar Power (mW)'], label='Solar')
+axs[1, 0].plot(df.index, df['Battery Power (mW)'], label='Battery')
+axs[1, 0].set_ylabel('Power (mW)')
+axs[1, 0].legend()
+axs[1, 0].set_title('Power over Time')
+
+# Turn off the unused subplot (bottom right)
+axs[1, 1].axis('off')
 
 # Format x-axis to show time
-for ax in axs:
+for ax in axs.flat:
     ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
 
 # Rotate and align the tick labels so they look better
