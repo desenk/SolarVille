8034143a575ff3dc45b00b89f96a34be410f2355
ArifAkanda <s2605573@ed.ac.uk>
2024-08-02 12:55:51 +0100

    changed IP

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import socket
 
-PI_1_IP = '192.168.245.200'
-PI_2_IP = '192.168.245.64'
+PI_1_IP = '192.168.85.200' # Ip of pi 1
+PI_2_IP = '192.168.85.63' #IP of Pi 2
 
 def get_local_and_peer_ip():
     hostname = socket.gethostname()
