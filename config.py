ee85d455ee9d27c954e8aee8e3878c5f2f2e3810
ArifAkanda <s2605573@ed.ac.uk>
2024-10-14 15:41:52 +0100

    Changed IP logic

--- config.py
+++ config.py
@@ -1,19 +1,36 @@
-import socket
+import netifaces # type: ignore
 
-#IP for hot spot
-#PI_1_IP = '192.168.137.85' # IP of Pi 1
-#PI_2_IP = '192.168.137.142' #IP of Pi 2
+PI_1_IP = '10.126.46.162'  # IP of Pi 1
+PI_2_IP = '10.126.50.50'  # IP of Pi 2
 
-# IP for eduroam remote control
-PI_1_IP = '10.126.46.162' # IP of Pi 1
-PI_2_IP = '10.126.50.50' # IP of Pi 2
+def get_network_ip():
+    # Get the non-loopback IP address of the machine.
+    try:
+        # Get all network interfaces
+        interfaces = netifaces.interfaces()
+        for interface in interfaces:
+            # Skip loopback interface
+            if interface == 'lo':
+                continue
+            addrs = netifaces.ifaddresses(interface)
+            if netifaces.AF_INET in addrs:
+                ip = addrs[netifaces.AF_INET][0]['addr']
+                if ip != '127.0.0.1':
+                    print(f"Network IP: {ip}")
+                    return ip
+    except Exception as e:
+        print(f"Error getting network IP: {e}")
+    return None
 
 def get_local_and_peer_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    if local_ip == PI_1_IP:
-        return PI_1_IP, PI_2_IP
-    else:
-        return PI_2_IP, PI_1_IP
+    local_ip = get_network_ip()
+    if local_ip:
+        if local_ip == PI_1_IP:
+            return PI_1_IP, PI_2_IP
+        elif local_ip == PI_2_IP:
+            return PI_2_IP, PI_1_IP
 
 LOCAL_IP, PEER_IP = get_local_and_peer_ip()
+
+print(f"Local IP: {LOCAL_IP}")
+print(f"Peer IP: {PEER_IP}")
