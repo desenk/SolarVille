c6f81206da35cca3a97ab873e6aa42a2eb227fdc
Gc810 <guochun810@gmail.com>
2024-10-14 13:49:12 +0100

    update

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import requests
 from config import LOCAL_IP, PEER_IP
 
-def send_data_to_peer(data):
-    try:
-        response = requests.post(f'http://{PEER_IP}:5000/update_peer_data', json=data)
-        if response.status_code == 200:
-            print("Data sent successfully!")
-        else:
-            print(f"Failed to send data. Status code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error sending data: {e}")
-
 def fetch_data_from_peer():
     try:
         response = requests.get(f'http://{PEER_IP}:5000/get_peer_data')
@@ -23,12 +13,5 @@ def fetch_data_from_peer():
         print(f"Error fetching data: {e}")
 
 if __name__ == "__main__":
-    # 测试数据上传
-    test_data = {
-        'demand': 10,
-        'generation': 5,
-        'balance': -5,
-        'battery SoC': 0.5,
-        'enable': 1
-    }
+   
     fetch_data_from_peer()
