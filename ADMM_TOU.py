9fd41fab0bc6c8deb1606d5e662ba258180ebd1f
Gc810 <guochun810@gmail.com>
2025-02-12 16:11:38 +0000

    Update ADMM_TOU.py
    AssertionError: daemonic processes are not allowed to have children

--- ADMM_TOU.py
+++ ADMM_TOU.py
@@ -7,7 +7,7 @@ from cvxpy import Minimize, Problem, Variable, norm, square
 from datetime import datetime, timedelta
 from multiprocessing import Pool
 from multiprocessing import Manager
-
+multiprocessing.set_start_method('spawn')
 class HouseholdADMM_CVXPY:
     def __init__(self, total_time_steps, household_ids, household_id, base_load, gen, battery_capacity, charge_power_limit, discharge_power_limit,
                  ev_max_capacity, ev_initial_soc, daily_prices, rho=0.1):
