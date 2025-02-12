0540ebd24a1451911e9e4b30807640d452a6655c
Gc810 <guochun810@gmail.com>
2025-02-12 11:04:45 +0000

    Update ADMM_TOU.py

--- ADMM_TOU.py
+++ ADMM_TOU.py
@@ -62,9 +62,9 @@ class HouseholdADMM_CVXPY:
         # **目标函数：最小化购电成本**
         objective = cvx.Minimize(
             cvx.sum([import_energy[t] * cvx.Constant(self.daily_prices[t]['import_price']) for t in range(self.T)]) +
-            cvx.sum([trade_in[(self.h, h2, t)] * self.lambda_trade for h2 in households.keys() for t in range(self.T)]) -  # 购买交易电的成本
+            cvx.sum([trade_in[(self.h, h2, t)] * cvx.Constant(float(self.lambda_trade)) for h2 in households.keys() for t in range(self.T)]) -  # 购买交易电的成本
             cvx.sum([export_energy[t] * cvx.Constant(self.daily_prices[t]['export_price']) for t in range(self.T)]) - 
-            cvx.sum([trade_out[(self.h, h2, t)] * self.lambda_trade for h2 in households.keys() for t in range(self.T)]) +  # 卖电的收益
+            cvx.sum([trade_out[(self.h, h2, t)] * cvx.Constant(float(self.lambda_trade)) for h2 in households.keys() for t in range(self.T)]) +  # 卖电的收益
             cvx.sum([self.penalty * self.ev_max_capacity * (1 - ev_soc[t]) for t in range(self.T)])  # 确保常量部分使用 cvx.Constant
             )
 
